c12f637015e6cdb8ca5af0006d30f5bc67dbbb10
Ivan Petrov <Ivan.Petrov@forsta.com>
2022-03-17 15:05:22 +0300

    SB-141 feat: add new field

--- Vacation Planner/Models/Employee.cs
+++ Vacation Planner/Models/Employee.cs
@@ -4,6 +4,7 @@ namespace VacationPlanner.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public List<Vacation> Vacations { get; set; }
     }
