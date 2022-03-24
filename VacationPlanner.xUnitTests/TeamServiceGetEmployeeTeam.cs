d3bc5ce342f57f22d9201b296a14475fdc8c501b
Ivan Petrov <Ivan.Petrov@forsta.com>
2022-03-24 11:01:35 +0300

    SB-143 test: also check vacations returned from GetEmployeeTeam method

--- VacationPlanner.xUnitTests/TeamServiceGetEmployeeTeam.cs
+++ VacationPlanner.xUnitTests/TeamServiceGetEmployeeTeam.cs
@@ -14,16 +14,19 @@ namespace VacationPlanner.xUnitTests
     {
         private TeamService teamService;
         private StubDbService StubDbService;
+        private DateTime currentDate;
 
         public TeamServiceGetEmployeeTeam()
         {
-            var currentDate = DateTime.Now;
+            currentDate = DateTime.Now;
             StubDbService = new StubDbService(new List<DataEmployee>());
             teamService =
                 new TeamService(
                     StubDbService);
             StubDbService.Employees.Add(new DataEmployee(0, "user 0", new List<DataVacation>(),
                 EmployeeRole.SoftwareEngineer, 0));
+            StubDbService.Employees[0].Vacations.Add(new DataVacation(0, currentDate.AddDays(10),
+                currentDate.AddDays(20), VacationState.Pending, 0));
             StubDbService.Employees.Add(new DataEmployee(1, "user 1", new List<DataVacation>(),
                 EmployeeRole.TeamLead, 0));
         }
@@ -33,6 +36,8 @@ namespace VacationPlanner.xUnitTests
         {
             var expectedTeam = new List<Employee>();
             expectedTeam.Add(new Employee(0, "user 0", new List<Vacation>(), EmployeeRole.SoftwareEngineer));
+            expectedTeam[0].Vacations
+                .Add(new Vacation(currentDate.AddDays(10), currentDate.AddDays(20), VacationState.Pending));
             expectedTeam.Add(new Employee(1, "user 1", new List<Vacation>(), EmployeeRole.TeamLead));
 
             var actualTeam = teamService.GetEmployeeTeam(0);
